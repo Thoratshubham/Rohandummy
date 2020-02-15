1af4a8b690034def64a5cc586ede9e142c3d5c1a
Your Name <shubhamthoratjuly1999@gmail.com>
2020-02-15 11:59:51 +0530

    Slider with moreinfo all constraints

---
+++ app/src/main/java/me/shubhamthorat/omkar/MainActivity.java
@@ -0,0 +1,38 @@
+package me.shubhamthorat.omkar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper v_flipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int image[] ={R.drawable.rohan,R.drawable.rohan};
+
+        v_flipper = findViewById(R.id.v_flipper);
+
+        for(int i=0; i < image.length;i++){
+            flipperImage(image[i]);
+        }
+    }
+
+    public void flipperImage(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(4000);
+        v_flipper.setAutoStart(true);
+
+        v_flipper.setInAnimation(this,android.R.anim.slide_in_left);
+        v_flipper.setInAnimation(this,android.R.anim.slide_out_right);
+    }
+}
---
+++ app/src/main/java/me/shubhamthorat/omkar/Moreinfo.java
@@ -0,0 +1,14 @@
+package me.shubhamthorat.omkar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Moreinfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moreinfo);
+    }
+}
